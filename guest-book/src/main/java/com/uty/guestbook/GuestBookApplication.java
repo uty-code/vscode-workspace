ec98d6ea001e90baf249d95502f1f084706d4b03
uty <sewo1218@gmail.com>
2026-03-26 14:44:43 +0900

    First commit message

---
+++ guest-book/src/main/java/com/uty/guestbook/GuestBookApplication.java
@@ -0,0 +1,13 @@
+package com.uty.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBookApplication.class, args);
+	}
+
+}
